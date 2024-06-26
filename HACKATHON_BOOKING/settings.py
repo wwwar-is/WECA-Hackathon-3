600d07186f6365c92af285bdb3060f4569993aee
Waris Gaspard <warisgaspard@gmail.com>
2024-06-26 10:39:21 +0000

    initial setup, incomplete

--- HACKATHON_BOOKING/settings.py
+++ HACKATHON_BOOKING/settings.py
@@ -18,6 +18,8 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR,
+'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'BOOKING_SYSTEM',
 ]
 
@@ -59,7 +63,7 @@ ROOT_URLCONF = 'HACKATHON_BOOKING.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,
+'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://uoakmrqkale:3fP8XohR6uoO@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mower_grab_large_118223"
 os.environ["SECRET_KEY"] = "bimbi"
+os.environ["CLOUDINARY_URL"] = "cloudinary://:@drslykauc"
