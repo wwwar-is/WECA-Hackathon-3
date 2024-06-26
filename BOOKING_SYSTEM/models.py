356101599c73596b3673eedad5c5dc23dd01b91a
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-06-26 13:18:14 +0000

    created customer model

--- BOOKING_SYSTEM/models.py
+++ BOOKING_SYSTEM/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
