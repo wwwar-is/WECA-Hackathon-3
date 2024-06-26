92f8e02112e2d16a8590b0ad1efd6f6a7493b3d7
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-06-26 13:57:26 +0000

    added all models to BOOKING_SYSTEM

--- BOOKING_SYSTEM/models.py
+++ BOOKING_SYSTEM/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Customer(models.model):
+class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=254, unique=True)
     phone_number = models.CharField(max_length=15, blank=True)
@@ -12,3 +12,38 @@ class Customer(models.model):
     def __str__(self):
         return self.user.username
 
+class Booking(models.Model):
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    booking_date = models.DateField()
+    booking_time = models.TimeField()
+    booking_length = models.CharField
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pending'),
+            ('confirmed', 'Confirmed'),
+            ('cancelled', 'Cancelled')
+        ],
+        default='pending'
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - on {self.booking_date} at {self.booking_time} for {self.booking_length}"
+    
+class Payment(models,Model):
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
+    quote = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pending'),
+            ('completed', 'Completed'),
+            ('failed', 'Failed')
+        ],
+        default='pending'
+    )
+    transaction_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"Payment for Booking {self.booking.id} - {self.status}"
