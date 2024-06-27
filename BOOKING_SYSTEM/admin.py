a7336fe4ebba0d9ac8c2e12237e427a29b597a6d
Sian Jones <sianwroejones@gmail.com>
2024-06-27 13:06:51 +0000

    registered models in admin.py

--- BOOKING_SYSTEM/admin.py
+++ BOOKING_SYSTEM/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Booking, Payment, Review
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(Review)
