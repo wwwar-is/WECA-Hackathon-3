ee48cc11862946f1d3dfc73f0e447d77fc5e7e4f
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-06-27 16:02:49 +0000

    added register view and template and linked up url paths

--- BOOKING_SYSTEM/urls.py
+++ BOOKING_SYSTEM/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.booking, name="booking")
+    path('register/', views.register_view, name='register'),
+    path('booking/', views.booking_view, name="booking"),
+    path('booking/confirmation/<int:booking_id>/', views.booking_confirmation_view, name='booking_confirmation'),
 ]
