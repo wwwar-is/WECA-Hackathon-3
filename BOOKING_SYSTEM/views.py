8f0dfcd7cd663ed57fa7a70c81400a2b5d0b5fcc
Sian Jones <sianwroejones@gmail.com>
2024-06-27 12:12:45 +0000

    setting up urls and views for BOOKING_SYSTEM

--- BOOKING_SYSTEM/views.py
+++ BOOKING_SYSTEM/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Customer, Booking, Payment, Review
 # Create your views here.
 
+def booking (request):
+    return HttpResponse("hello world!")
--- HACKATHON_BOOKING/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def booking(request):
-    return HttpResponse('booking')
-
-def signup(request):
-    return HttpResponse('signup')  
-  
